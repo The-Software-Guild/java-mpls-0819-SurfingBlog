a2c72bf525570de47953ac6d9d7d235ccb887b79
blee025 <blee025@gmail.com>
2019-10-17 13:22:50 -0500

    webpage

--- Security/surfsecurity.sql
+++ Security/surfsecurity.sql
@@ -21,6 +21,31 @@ primary key(`user_id`,`role_id`),
 foreign key (`user_id`) references `user`(`id`),
 foreign key (`role_id`) references `role`(`id`));
 
+create table Beaches (
+`id` 		int 			primary key auto_increment,
+`name` 		varchar(30)		not null,
+`zipcode` 	int(5) 			not null,
+`breakid` 	int 			not null, 
+
+foreign key (`breakid`) references Breaks(`id`)
+
+);
+
+create table BeachComments (
+`id`		int 			primary key auto_increment,
+userId		int 			not null,
+beachid		int 			not null,
+comment		varchar(1000)	not null,
+
+foreign key (userId) references `user`(`id`),
+foreign key (beachId) references Beaches(`id`)
+
+);
+
+create table Breaks (
+`id`		int 			primary key
+);
+
 insert into `user`(`id`,`username`,`password`,`enabled`)
     values(1,"admin", "$2a$10$Pwyf0jn0glXZ36Nvo0GBtuUdl0Y5OoXV7izp2/Mi6YGvx3YY4Zcmi", true),
         (2,"user","$2a$10$Pwyf0jn0glXZ36Nvo0GBtuUdl0Y5OoXV7izp2/Mi6YGvx3YY4Zcmi",true);
