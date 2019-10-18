cad3969737154f64f0d1dc075a4ee89aac8742f0
blee025 <blee025@gmail.com>
2019-10-18 15:56:01 -0500

    minor update to remove jpa imports

--- Security/surfingblog/src/main/java/sg/surfingblog/newpackage/models/Beach.java
+++ Security/surfingblog/src/main/java/sg/surfingblog/newpackage/models/Beach.java
@@ -5,13 +5,6 @@
  */
 package sg.surfingblog.newpackage.models;
 
-import java.util.List;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
 /**
  *
  * @author Chad
--- Security/surfingblog/src/main/java/sg/surfingblog/newpackage/models/Role.java
+++ Security/surfingblog/src/main/java/sg/surfingblog/newpackage/models/Role.java
@@ -6,11 +6,6 @@
 package sg.surfingblog.newpackage.models;
 
 import java.util.Objects;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 /**
  *
