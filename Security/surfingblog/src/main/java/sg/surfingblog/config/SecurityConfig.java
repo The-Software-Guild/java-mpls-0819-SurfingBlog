e765220a4f43cfa9504820dbd97dcb853b906ebf
chadmcg <chadmcg@gmail.com>
2019-10-28 12:03:52 -0500

    beach page updates

--- Security/surfingblog/src/main/java/sg/surfingblog/config/SecurityConfig.java
+++ Security/surfingblog/src/main/java/sg/surfingblog/config/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .logoutSuccessUrl("/")
                     .permitAll();
         
-//        http.csrf().disable();
+        http.csrf().disable();
     }
     
      @Autowired
